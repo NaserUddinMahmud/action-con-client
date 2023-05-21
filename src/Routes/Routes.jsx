0b5ed9a426e78692965501a0e869b5445f56f8d3
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-05-21 17:29:37 +0600

    404 page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,12 +6,14 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register"
 import AddAToy from "../pages/AddAToy/AddAToy";
 import AllToys from "../pages/AllToys/AllToys";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
             path: '/',
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,33 @@
+import { Link, useRouteError,} from 'react-router-dom'
+
+const ErrorPage = () => {
+  const { error, status } = useRouteError()
+  return (
+    <section className='flex items-center h-screen p-16  text-gray-900'>
+        
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        
+        
+        <div className='max-w-xs text-center'>
+        <div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/61lO1Gn6WNL._AC_UL600_SR600,600_.jpg" alt="" />
+        </div>
+          <h2 className='mb-8 font-extrabold text-5xl text-gray-600'>
+            <span className='sr-only'>Error</span> {status || 404}
+          </h2>
+          <p className='text-2xl font-semibold md:text-3xl mb-8'>
+            {error?.message}
+          </p>
+          <Link
+            to='/'
+            className='btn btn-outline px-8 py-3 font-semibold rounded-2xl border-error text-gray-900'
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ErrorPage
