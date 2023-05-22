a3818cdb2445d64c0310b842f920615ded2d0c3c
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-05-23 03:16:53 +0600

    footer updated

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
 import logo from '../../../../public/ActionCon.png'
+import { FaPhoneAlt, FaRegEnvelope,FaStore, FaFacebookF,FaTwitter,FaYoutube,FaInstagram } from "react-icons/fa";
+
 
 
 const Footer = () => {
@@ -9,25 +12,32 @@ const Footer = () => {
   <img className="w-52 -ml-4" src={logo} alt="" />
     <p>ActionCon Ltd.<br/>Copyright © 2023 - All right reserved by ActionCon</p>
   </div> 
+  
   <div>
-    <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
+    <span className="footer-title">Links</span> 
+    <Link to={'/toys'} className="link link-hover">All Toys</Link> 
+    <Link to={'/myToys'} className="link link-hover">My Toys</Link> 
+    <Link to={'/addAToy'} className="link link-hover">Add A toy</Link> 
+    <Link to={'/blogs'} className="link link-hover">Blogs</Link>
   </div> 
   <div>
-    <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
-  </div> 
+    <span className="footer-title">CONTACT INFORMATION</span> 
+    <p className='flex items-center'><FaStore></FaStore> {' '}ActionCon Store</p>
+    <p>67 Boulevard London, Paris,
+United States</p>
+    <p className='flex items-center'><FaPhoneAlt></FaPhoneAlt> +1234567890</p>
+    
+    <p className='flex items-center'><FaRegEnvelope></FaRegEnvelope> mail@actioncon.Com</p>
+  </div>
   <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
+    <span className="footer-title">Social Media</span> 
+    
+      <Link className='flex items-center link link-hover'><FaFacebookF></FaFacebookF>Facebook</Link> 
+      <Link className='flex items-center link link-hover'><FaTwitter></FaTwitter>Twitter</Link> 
+      <Link className='flex items-center link link-hover'><FaYoutube></FaYoutube>Youtube</Link> 
+      <Link className='flex items-center link link-hover'><FaInstagram></FaInstagram>Instagram</Link> 
+      
+    
   </div>
 </footer>
         </div>
